aa7d63f2527f955fe91f93fb84fd7adbead7ebbf
Jacques HERRMANN <jacquesherrmann@gmail.com>
2021-01-19 10:49:36 +0100

    Hello Word

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <div id="app">Hello World</div>
+</template>
+
+<script>
+    export default {}
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
-const path = require('path') // path resolution
-const webpack = require('webpack')
+const path = require('path'); // path resolution
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let config = {
-    entry: '.src/main.js', // entrypoint
+    mode: 'development',
+    entry: './src/main.js', // entrypoint
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist',
+        // publicPath: 'dist',
         filename: 'main.js'
     },
     resolve: {
         extensions: ['.js', '.vue']
     },
-    devServer: {
-        noInfo: true, // Hide running information from terminal
-    },
     module: { // loader list
-        rules: [{ // resolve .vue file with vue-loader
-            test: /\.vue$/,
-            loader: 'vue-loader'
-        }]
-    }
-}
-module.exports = config
+        rules: [
+            { // resolve .js file with babel
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            { // resolve .vue file with vue-loader
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+        ]
+    },
+    plugins: [ // Vue and hot module plugin
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+        }),
+        new VueLoaderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+};
+module.exports = config;
